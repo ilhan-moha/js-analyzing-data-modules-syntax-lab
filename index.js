a741aced16cceeb743b809425d11595bdd8a7c66
ilhan-moha <mohamudilhan99@gmail.com>
2026-04-16 07:13:36 +0300

    data modules syntax lab

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+const datejs = require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+     users:[]
+  };
+  for (let arr of args) {
+    combinedObject.users =[
+      ...combinedObject.users,
+      ...arr
+    ]
+  };
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+  return combinedObject;
+}
 
+console.log(combineUsers(
+  [
+   'halima',
+   'mohamed',
+   'sara'
+  ],
+  [
+    'ahmed',
+    'yussuf',
+    'salma'
+  ],
+  [
+   'omar',
+   'nour',
+   'laila'
+  ]
+));
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
